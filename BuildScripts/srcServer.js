2a67a7e4f523330480bbfdd904b7c790ce0130e8
Tangrila-BG <phatomiki@gmail.com>
2017-04-26 17:04:34 +0300

    added transpiling - babel

--- BuildScripts/srcServer.js
+++ BuildScripts/srcServer.js
@@ -1,9 +1,9 @@
-let express = require('express');
-let path = require('path');
-let open = require('open');
+import express from require('express');
+import path    from require('path');
+import open    from require('open');
 
-let port = 3000;
-let app = express();
+const port = 3000;
+const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'));
